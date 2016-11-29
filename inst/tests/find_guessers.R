32c9f2dd5f0df44583e39a59dc3088afb64f54d2
Borysław Paulewicz <borys@localhost.localdomain>
2016-11-29 14:56:35 +0100

    test find_guessers

---
+++ inst/tests/find_guessers.R
@@ -0,0 +1,11 @@
+library(bp)
+## library(runjags)
+## library(coda)
+library(ggplot2)
+
+score = c(21,17,21,18,22,31,31,34,34,35,35,36,39,36,35)
+n = rep(40, length(score))
+res = find_guessers(score, n)
+round(coda::effectiveSize(res$fit))
+
+plot_guessers(res)
