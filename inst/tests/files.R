7b6d0d7da2462adf5d1c1402d7e414875b5f7eb4
Borysław Paulewicz <borys.paulewicz@gmail.com>
2016-11-30 22:32:23 +0100

    Poprawiony test do_files

--- inst/tests/files.R
+++ inst/tests/files.R
@@ -1,5 +1,9 @@
 do_files_test = function(dir){
-  test = function(f){ res = !isdir(f); res; }
+  test = function(f){ res = !is_dir(f); res; }
   fun = function(f){ message(f); }
   do_files(test = test, fun = fun, dir);
 }
+
+## Powinien pokazać wszystkie pliki w podkatalogach, bez nazw
+## katalogów
+do_files_test('../')
